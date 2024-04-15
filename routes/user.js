b2ef493ef9cc823b9ea1a5366beae65f42e05e7d
Kaushal Sindhu <kaushal1308sindhu@gmail.com>
2024-04-15 21:18:09 +0530

    commit

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({mergeParams : true});
-const wrapAsync = require("../utils/wrapasync.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const {saveRedirectUrl} = require("../middleware.js");
 const userController = require("../controllers/users.js");
